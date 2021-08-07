48d523de54a0807b475344882c07baee03e09926
Guillaume D. Gth <guillaume.zarb@gmail.com>
2021-08-07 21:03:49 +0200

    map integration

--- lib/map.dart
+++ lib/map.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:ansicolor/ansicolor.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,31 +11,90 @@ class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
-
+class Logger{
+  static log(msg) {
+    AnsiPen pen = new AnsiPen()..red();
+    print(pen('$msg'));
+  }
+}
 class _MyAppState extends State<MyApp> {
+  late String _mapStyle;
+  late LatLng? _center;
+  late Position position;
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-
   void _onMapCreated(GoogleMapController controller) {
+    print("gza");
     mapController = controller;
+    print(_mapStyle);
+    mapController.setMapStyle(_mapStyle);
   }
-
+  @override
+  void initState() {
+    super.initState();
+    loadStyle();
+    getLocation();
+  }
+  loadStyle() async {
+    await rootBundle.loadString('assets/styles/map_style.txt').then((string) {
+        setState(() {
+          _mapStyle = string;
+        });
+      });
+  }
+  getLocation () async {
+    if (await Permission.location.request().isGranted) {
+      position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      setState(() {
+        _center = LatLng(position.latitude, position.longitude);
+      });
+    }
+  }
+  @override
+  void dispose(){
+    mapController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
+    List<Marker> customMarkers = [];
+    Marker m = Marker(
+        markerId: MarkerId("UserMarker"),
+        position: _center as LatLng
+    );
+    customMarkers.add(m);
+    Widget w;
+    if (_center == null){
+      w = Text("Loading");
+    }
+    else {
+      print(_center);
+      w = GoogleMap(
+        markers: customMarkers.toSet(),
+        zoomControlsEnabled: false,
+        scrollGesturesEnabled: false,
+        mapType: MapType.normal,
+        minMaxZoomPreference: MinMaxZoomPreference(17, 19),
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center as LatLng,
+          zoom: 18.0,
+        ),
+      );
+    }
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Maps Sample App'),
+          title: Text('Fläg'),
           backgroundColor: Colors.green[700],
+            actions: <Widget>[
+              IconButton(
+                iconSize: 45,
+                icon: const Icon(Icons.menu),
+                onPressed: () {},
+              )]
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
+        body: w
       ),
     );
   }
